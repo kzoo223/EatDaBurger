949b1f940ec3d7cd0c9dc8dfcb7919b9360e6c88
Kevin Zhu <kzoo223@gmail.com>
2016-11-05 23:29:34 -0400

    initial setup files

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Bacon Cheeseburger', false, now());
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('BLT Burger', false, now());
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Buffalo Burger', false, now());
---
+++ server.js
@@ -0,0 +1,4 @@
+var bodyParser = require('body-parser');
+var methodOR = require('method-override')
+var express = require('express');
+var app = express();
