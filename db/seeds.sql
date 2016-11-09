da3f319aed30c2ad92d195a168f1f17a4a562029
Kevin Zhu <kzoo223@gmail.com>
2016-11-08 20:55:53 -0500

    controller updated

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
 USE burgers_db;
 
-INSERT INTO burgers (burger_name, devoured, date) VALUES ('Bacon Cheeseburger', false, now());
-INSERT INTO burgers (burger_name, devoured, date) VALUES ('BLT Burger', false, now());
-INSERT INTO burgers (burger_name, devoured, date) VALUES ('Buffalo Burger', false, now());
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BLT Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Buffalo Burger', false);
---
+++ models/burger.js
@@ -0,0 +1,24 @@
+var orm = require('../config/orm.js');
+
+var burger = {
+  all: function(callback){
+    orm.selectAll("burgers" , function(res){
+      callback(res);
+    });
+  },
+
+  create: function(callback){
+    orm.insertOne("burgers", cols, vals, function (res){
+      callback(res);
+    });
+  },
+
+  update: function(objColVals, condition, callback){
+    orm.updateOne("burgers", objColVals, condition, function (res){
+      cb(res);
+    });
+  }
+
+};
+
+module.exports = burger;
